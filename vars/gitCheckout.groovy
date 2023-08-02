1a2a09adc4357a5f18e72e7133e9620b3a30630a
Guddu Singh <gs862863864@gmail.com>
2023-08-02 11:24:02 +0530

    git checkout file

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,9 @@
+def call(Map stageParams) {
+    
+    checkout ({
+        $class: 'GitSCM',
+        branches: [[ name: stageParams.branch ]],
+        userRemotConfigs: [[ url: stageParams.url ]] 
+
+    })
+}
